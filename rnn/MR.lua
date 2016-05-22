ef2050110ed7c29df2b29b63c944ee8ffbed753a
Jin Hwa Kim <jnhwkim@gmail.com>
2016-05-22 13:37:56 +0900

    around 74%.

---
+++ rnn/MR.lua
@@ -0,0 +1,87 @@
+---------------------------------------------------------------------
+-- Preprocessor for Movie Reivew Data v1.0 sentence polarity dataset comes
+-- from the URL
+-- http://www.cs.cornell.edu/people/pabo/movie-review-data .
+--
+-- Author: Jin-Hwa Kim (jnhwkim@snu.ac.kr)
+-- License: BSD 3-Clause
+---------------------------------------------------------------------
+
+-- Options
+if not opt then opt = {} end
+opt.data_path = 'rt-polaritydata/rt-polarity'
+opt.types = {'pos','neg'}
+
+-- Dataset
+local rtp = {}
+rtp.word_to_idx = {}
+rtp.idx_to_word = {}
+rtp.word_freq = {}
+rtp.nVocabulary = 0
+rtp.nSample = 0
+rtp.max_length = -1
+
+print('Build vocabulary table..')
+for _idx,t in pairs(opt.types) do
+	local f = io.open(opt.data_path..'.'..t)
+	while true do
+		local line = f:read()
+		if line == nil then break end
+		rtp.nSample = rtp.nSample and rtp.nSample + 1 or 1
+		local tokens = line:split(' ')
+		rtp.max_length = math.max(rtp.max_length, #tokens)
+		for _idx,w in pairs(tokens) do
+			local widx = rtp.word_to_idx[w]
+			local wfreq = rtp.word_freq[w]
+			if not widx then
+				rtp.nVocabulary = rtp.nVocabulary + 1
+				rtp.word_to_idx[w] = rtp.nVocabulary
+				widx = rtp.nVocabulary
+				rtp.idx_to_word[widx] = w
+			end
+			rtp.word_freq[w] = wfreq and wfreq + 1 or 1
+		end
+	end
+	f:close()
+end
+
+print('\nStatistics:')
+print('number of samples = ', rtp.nSample)
+print('number of vocabulary = ', rtp.nVocabulary)
+print('maximum sentence length = ', rtp.max_length)
+
+print('\nBuild data tensors..')
+rtp.X = torch.IntTensor(rtp.nSample, rtp.max_length):zero()
+rtp.y = torch.LongTensor(rtp.nSample):fill(1)  -- negative
+rtp.y:narrow(1,1,rtp.nSample/2):fill(2)  -- positive
+
+local i = 0
+for _idx,t in pairs(opt.types) do
+	local f = io.open(opt.data_path..'.'..t)
+	while true do
+		local line = f:read()
+		if line == nil then break end
+		i = i + 1
+		local tokens = line:split(' ')
+		for j,w in pairs(tokens) do
+			local widx = rtp.word_to_idx[w]
+			rtp.X[i][rtp.max_length+j-#tokens] = widx  -- right-aligned population
+		end
+	end
+	f:close()
+end
+
+collectgarbage()
+
+print('\nTensor Information:')
+require 'torchx'  -- provides `find`
+print('zero ratio = ', #torch.find(rtp.X, 0)/rtp.X:nElement())
+
+-- Save results
+print('\nSave results as t7 file..')
+torch.save('rtp.t7', rtp)
+
+print('\nMovie Review Dataset loaded.\n')
+
+-- Return results
+return rtp
---
+++ rnn/train.lua
@@ -0,0 +1,124 @@
+---------------------------------------------------------------------
+-- Training code for Movie Reivew Data v1.0 sentence polarity dataset comes
+-- from the URL
+-- http://www.cs.cornell.edu/people/pabo/movie-review-data .
+--
+-- Author: Jin-Hwa Kim (jnhwkim@snu.ac.kr)
+-- License: BSD 3-Clause
+---------------------------------------------------------------------
+
+require 'rnn'
+require 'optim'
+
+-- Training Options
+opt = {}
+opt.cv = 10
+opt.hiddenSize = 100
+opt.batchSize = 64
+opt.learningRate = .001
+opt.maxEpoch = 50
+opt.cuda = true
+torch.manualSeed(123)
+
+if opt.cuda then 
+   require 'cutorch'
+   require 'cunn'
+end
+
+MR = require 'MR'  -- load Movie Review dataset
+if opt.cuda then
+   MR.X = MR.X:cuda()
+   MR.y = MR.y:cuda()
+end
+
+-- Cross Validation
+function cv(nSample, n)
+   local idx = torch.randperm(nSample)
+   local ssize = math.floor(nSample/n)
+   local idx_train = torch.LongTensor(n, nSample-ssize)
+   local idx_val = torch.LongTensor(n, ssize)
+   for i=1,n do
+      local pos1, pos2 = ssize*(i-1)+1, ssize*i+1
+      if i ~= 1 then 
+         idx_train[i][{{1,pos1-1}}]:copy(idx[{{1,pos1-1}}])
+      end; if i ~= n then 
+         idx_train[i][{{pos1,idx_train:size(2)}}]:copy(idx[{{pos2,idx:size(1)}}])
+      end
+      idx_val[i]:copy(idx:narrow(1,pos1,ssize))
+   end
+   return idx_train, idx_val
+end
+idx_train, idx_val = cv(MR.nSample, opt.cv)  -- 10-fold cross validation
+
+-- Model definition
+rnnModule = nn.GRU(opt.hiddenSize, opt.hiddenSize, nil, .25):maskZero(1)
+model = nn.Sequential()
+   :add(nn.LookupTableMaskZero(MR.nVocabulary, opt.hiddenSize))
+   :add(nn.SplitTable(2))
+   :add(nn.Sequencer(rnnModule))
+   :add(nn.SelectTable(-1))
+   :add(nn.Dropout(.5))
+   :add(nn.Linear(opt.hiddenSize, 2))
+
+-- Criterion
+criterion = nn.CrossEntropyCriterion()
+
+if opt.cuda then
+   model = model:cuda()
+   criterion = criterion:cuda()
+end
+w,dw = model:getParameters()
+
+function JDJ(_w)
+   w:copy(_w)
+   dw:zero()
+   local output = model:forward(X)
+   J = criterion:forward(output, y)
+   local dy = criterion:backward(output, y)
+   model:backward(X, dy)
+   dw:clamp(-10,10)
+   return J,dw
+end
+
+function evaluate(X,y)
+   model:evaluate()
+   output = model:forward(X)
+   _max,pred = torch.max(output,2)
+   local diff = pred-y:resizeAs(pred)
+   require 'torchx'
+   model:training()
+   return #torch.find(diff, 0) / y:nElement()
+end
+
+local config={}
+config.learningRate=opt.learningRate
+config.maxIter=opt.maxEpoch
+config.update_grad_per_n_batches=1
+nSample = idx_train:size(2)
+
+for k=1,opt.cv do
+   w:uniform(-.08,.08)  -- init
+   config.winit = w
+   local X_test = MR.X:index(1,idx_val[k])
+   local y_test = MR.y:index(1,idx_val[k])
+   local accuracy = evaluate(X_test, y_test)
+   print(string.format('CV-%d Accuracy = %.2f', k, accuracy))
+   for epoch=1,opt.maxEpoch do
+      for i=1,nSample,opt.batchSize do
+         xlua.progress(i, nSample)
+         local j=math.min(i+opt.batchSize-1,nSample)
+         X = MR.X:index(1,idx_train[k][{{i,j}}])
+         y = MR.y:index(1,idx_train[k][{{i,j}}])
+         optim.adam(JDJ, config.winit, config)
+      end
+      xlua.progress(nSample, nSample)  -- done
+      print(string.format('Epoch#%2d\tJ = ', epoch), J)
+      local X_test = MR.X:index(1,idx_val[k])
+      local y_test = MR.y:index(1,idx_val[k])
+      local accuracy = evaluate(X_test, y_test)
+      print(string.format('CV-%d Accuracy = %.2f', k, accuracy))
+      collectgarbage()
+      if 0 == epoch % 15 then config.learningRate = config.learningRate / 2 end
+   end
+   break
+end
